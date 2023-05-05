5d76020d847a4d1f2f98fafde5fa668b6e011e55
arover <arnaud.rover@gmail.com>
2023-05-05 21:50:36 +0200

    Optimize performance

--- integration_tests/models/stg/add_clients.py
+++ integration_tests/models/stg/add_clients.py
@@ -5,10 +5,8 @@ from datetime import datetime
 
 def add_clients(session, nb, start):
     fake = Faker()
-    clients = []
-    start = start + 1
-    for value in range(nb):
-        clients.append([start + value, fake.first_name(), fake.last_name(), fake.date_of_birth(minimum_age=18, maximum_age=90), datetime.now()])
+    fake_client = [fake.first_name(), fake.last_name(), fake.date_of_birth(minimum_age=18, maximum_age=90)]
+    clients = [[value] + fake_client + [datetime.now()] for value in range(start, start + nb)]
     df = session.create_dataframe(clients, schema=["ID", "FIRST_NAME", "LAST_NAME", "BIRTHDATE", "CREATED_AT"])
     return df
 
@@ -21,7 +19,7 @@ def model(dbt, session):
     if dbt.is_incremental:
         # only new rows compared to max in current table
         max_id_req = f"select max(id) from {dbt.this}"
-        max_id= session.sql(max_id_req).collect()[0][0]
+        max_id= session.sql(max_id_req).collect()[0][0] 
 
     nb_clients = dbt.config.get("nb_clients")
-    return add_clients(session, nb_clients, int(max_id))
+    return add_clients(session, nb_clients, int(max_id) + 1)
