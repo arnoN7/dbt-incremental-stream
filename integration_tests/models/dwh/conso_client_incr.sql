378e8a28c52b039359874bdf5b8e029578370d8e
arover <arnaud.rover@gmail.com>
2023-05-05 21:39:37 +0200

    Add test to compare incremental_stream with incremental materialization

---
+++ integration_tests/models/dwh/conso_client_incr.sql
@@ -0,0 +1,19 @@
+{{-
+    config(
+        materialized='incremental',
+        unique_key=['ID']
+    )
+-}}
+with client_with_dup as (
+    SELECT row_number() OVER (PARTITION BY ID ORDER BY CREATED_AT DESC) as NB_DUP,
+    ID, FIRST_NAME, LAST_NAME, BIRTHDATE, CREATED_AT
+    FROM {{ref('add_clients')}}
+), clients_without_dup as (
+    SELECT ID, FIRST_NAME, LAST_NAME, BIRTHDATE, CREATED_AT FROM client_with_dup 
+    WHERE NB_DUP = 1 
+    {% if is_incremental() %}
+      and CREATED_AT >= coalesce((select max(CREATED_AT) from {{ this }}), '1900-01-01')
+    {% endif %}
+    ORDER BY CREATED_AT
+)
+SELECT * FROM clients_without_dup
