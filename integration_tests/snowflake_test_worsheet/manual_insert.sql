9972e19c4050d12f90d5db329cc4129c9bf5547b
arnoN7 <arnaud.rover@gmail.com>
2023-05-09 23:10:27 +0200

    Delete integration_tests/snowflake_test_worsheet directory

--- integration_tests/snowflake_test_worsheet/manual_insert.sql
+++
@@ -1,3 +0,0 @@
-select * from PERSO.ARO_STG.ADD_CLIENTS limit 10;
-insert into PERSO.ARO_STG.ADD_CLIENTS values (1, 'Daniel', 'Hanna', '1986-02-10', current_timestamp());
-CREATE STAGE PERSO.ARO_STG.CLIENTS;
