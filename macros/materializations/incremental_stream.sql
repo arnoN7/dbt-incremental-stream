ba171b3e942bfb31970f4baeac146c9a85447c31
arover <arnaud.rover@gmail.com>
2023-08-22 12:18:36 +0200

    Refactor code to drop temporary view

--- macros/materializations/incremental_stream.sql
+++ macros/materializations/incremental_stream.sql
@@ -7,12 +7,14 @@
 {#--    3) sql = SQL used for the transformation            --#}
 {#-- ###################################################### --#}
 
-{%- macro merge_stream_sql(target_relation, src, unique_key, sql) -%}
+{%- macro merge_stream_sql(target_relation, sql, tmp_relation, unique_key) -%}
 {%- set full_refresh_mode = (flags.FULL_REFRESH == True) -%}
 
 {#-- Get parameter values --#}
+{% set target_relation = this %}
 {%- set src_list = config.get('src_list') -%}
-{%- set unique_key = config.get('unique_key') -%}
+{% set grant_config = config.get('grants') %}
+{% set on_schema_change = incremental_validate_on_schema_change(config.get('on_schema_change'), default='ignore') %}
 {%- set streams = [] -%}
 {#-- Drop Target Table in full-refresh mode  --#}
 {% if full_refresh_mode -%} 
@@ -38,14 +40,12 @@
     CREATE STREAM IF NOT EXISTS {{target_stream}} ON TABLE {{source_table}};
 {% endfor %}
 
-{%- set target_view = this.database + '.' + this.schema + '.V_' + this.name -%}
+
 
 {%- set is_data_in_streams = 0 -%}
 {% set relation_exists = load_relation(this) is not none %}
 {%- if full_refresh_mode or not relation_exists%} 
-CREATE OR REPLACE VIEW {{target_view}}
-    AS ({{ sql }});
-CREATE TABLE IF NOT EXISTS {{ this }} AS SELECT * FROM {{target_view}};
+CREATE TABLE IF NOT EXISTS {{ this }} AS SELECT * FROM ({{sql}});
 {%- else -%}
 {#-- Check the presence of records to merge --#}
 {%- set is_data_in_streams -%}
@@ -72,16 +72,17 @@ CREATE TABLE IF NOT EXISTS {{ this }} AS SELECT * FROM {{target_view}};
 {#-- Execute Merge only if new records in stream  --#}
 {%- if is_data_in_streams  -%}
 {{ log("  => record(s) to merge" , info=True) }}
-CREATE OR REPLACE VIEW {{target_view}}
-    AS ({{ sql }});
+    {%- call statement('create_tmp_relation') -%}
+        {{ snowflake__create_view_as_with_temp_flag(tmp_relation, sql, True) }}
+    {%- endcall -%}
 
-    {%- set columns = adapter.get_columns_in_relation(target_view) | rejectattr('name', 'equalto', 'METADATA$ACTION')
+    {%- set columns = adapter.get_columns_in_relation(tmp_relation) | rejectattr('name', 'equalto', 'METADATA$ACTION')
                 | rejectattr('name', 'equalto', 'METADATA$ISUPDATE')
                 | rejectattr('name', 'equalto', 'METADATA$ROW_ID')
                 | list %}
     {%- if unique_key -%}
     MERGE INTO  {{ this }} TARGET
-    USING {{target_view}} SOURCE
+    USING {{tmp_relation}} SOURCE
     ON ({% for key in unique_key %}
     TARGET.{{ key }} = SOURCE.{{ key }}{%- if not loop.last %} AND {% endif %}{% endfor %}
     )
@@ -105,7 +106,7 @@ CREATE OR REPLACE VIEW {{target_view}}
     {% else %}
         INSERT INTO {{ this }}  
         SELECT {% for col in columns%}
-        SOURCE.{{col.name}}{%- if not loop.last %},{% endif %}{% endfor %} FROM {{target_view}} SOURCE WHERE SOURCE.METADATA$ACTION = 'INSERT' 
+        SOURCE.{{col.name}}{%- if not loop.last %},{% endif %}{% endfor %} FROM {{tmp_relation}} SOURCE WHERE SOURCE.METADATA$ACTION = 'INSERT' 
         AND SOURCE.METADATA$ISUPDATE = 'FALSE';
     {%- endif -%}
 
@@ -118,32 +119,31 @@ CREATE OR REPLACE VIEW {{target_view}}
 
 
 {%- materialization incremental_stream, adapter='snowflake' -%}
-  {%- set src = config.get('src') -%}
-  {%- set src_table = config.get('src_table') -%}
+    {% set target_relation = this %}
+    {%- set unique_key = config.get('unique_key') -%}
+    {% set incremental_strategy = config.get('incremental_strategy') or 'default' %}
+    {% set tmp_relation_type = dbt_snowflake_get_tmp_relation_type(incremental_strategy, unique_key, 'sql') %}
+    {% set tmp_relation = make_temp_relation(this).incorporate(type=tmp_relation_type) %}
+    {%- set existing_relation = load_relation(this) -%}
 
-  {%- set unique_key = config.get('unique_key') -%}
+    -- setup
+    {{ run_hooks(pre_hooks, inside_transaction=False) }}
 
-  {%- set target_relation = this -%}
-  {%- set existing_relation = load_relation(this) -%}
+    -- `BEGIN` happens here:
+    {{ run_hooks(pre_hooks, inside_transaction=True) }}
 
-  -- setup
-  {{ run_hooks(pre_hooks, inside_transaction=False) }}
+    {#-- Create the MERGE --#}
+    {%- set build_sql = incr_stream.merge_stream_sql(target_relation, sql, tmp_relation, unique_key) -%}
 
-  -- `BEGIN` happens here:
-  {{ run_hooks(pre_hooks, inside_transaction=True) }}
+    {%- call statement('main') -%}
+        {{ build_sql }}
+    {%- endcall -%}
 
-  {#-- Create the MERGE --#}
-  {%- set build_sql = incr_stream.merge_stream_sql(target_relation, src, unique_key, sql) -%}
+    -- `COMMIT` happens here
+    {{ adapter.commit() }}
+    {{ run_hooks(post_hooks, inside_transaction=False) }}
+    {% do drop_relation_if_exists(tmp_relation) %}
 
-  {%- call statement('main') -%}
-    {{ build_sql }}
-  {%- endcall -%}
-
-  -- `COMMIT` happens here
-  {{ adapter.commit() }}
-
-  {{ run_hooks(post_hooks, inside_transaction=False) }}
-
-  {{ return({'relations': [target_relation]}) }}
+    {{ return({'relations': [target_relation]}) }}
 
 {%- endmaterialization %}
